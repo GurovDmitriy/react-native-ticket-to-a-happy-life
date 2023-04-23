7d8dc1e4cf4e6b157ce1d2d64606358865360c08
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2023-04-23 18:23:24 +0300

    test: paper search

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query: any) => setSearchQuery(query);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.main}>
-        <Text>react native</Text>
-      </View>
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.main}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+          <Text>react native {searchQuery}</Text>
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
